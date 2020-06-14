28b3c88aeb31c6b4b0460ec2b786ba408edcd7d7
supersquamy <amyrobinsonlearns@gmail.com>
2020-06-14 21:46:17 +0100

    Day 3 #100DaysOfCode; finalised script and selected font for project

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
+        <link href="horoscopeStyles.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css2?family=Bellota:wght@300&display=swap" rel="stylesheet">
         <title>Horoscope calculator</title>
     </head>
     <body>
--- signScript.php
+++ signScript.php
@@ -12,26 +12,154 @@ function exceptionHandler($day, $month) {
         echo "Oops! Not sure that's a calendar day. Go back and try again!";
     } else {
         //run horoscopeCalc function with correct dates input
-        echo 'Running calculator..' . '<br>';
         horoscopeCalc($day, $month);
     }
 }
 
+//12 functions below, one per month, to calculate sign
 function january($day) {
     if ($day <= 20) {
         $sign = 'Capricorn';
     } else {
         $sign = 'Aquarius';
     }
-    echo $sign;
+    echo '<div>' . $sign . '</div>';
 }
 
-//This function calculates the sign of the month and date put in and returns it as a variable
+function february($day) {
+    if ($day <= 19) {
+        $sign = 'Aquarius';
+    } else {
+        $sign = 'Pisces';
+    }
+    echo "<div id='result'>" . $sign . "</div>";
+}
 
+function march($day) {
+    if ($day <= 20) {
+        $sign = 'Pisces';
+    } else {
+        $sign = 'Aries';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function april($day) {
+    if ($day <= 20) {
+        $sign = 'Aries';
+    } else {
+        $sign = 'Taurus';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function may($day) {
+    if ($day <= 20) {
+        $sign = 'Taurus';
+    } else {
+        $sign = 'Gemini';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function june($day) {
+    if ($day <= 21) {
+        $sign = 'Gemini';
+    } else {
+        $sign = 'Cancer';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function july($day) {
+    if ($day <= 22) {
+        $sign = 'Cancer';
+    } else {
+        $sign = 'Leo';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function august($day) {
+    if ($day <= 23) {
+        $sign = 'Leo';
+    } else {
+        $sign = 'Virgo';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function september($day) {
+    if ($day <= 21) {
+        $sign = 'Virgo';
+    } else {
+        $sign = 'Libra';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function october($day) {
+    if ($day <= 22) {
+        $sign = 'Libra';
+    } else {
+        $sign = 'Scorpio';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function november($day) {
+    if ($day <= 21) {
+        $sign = 'Scorpio';
+    } else {
+        $sign = 'Sagittarius';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+function december($day) {
+    if ($day <= 21) {
+        $sign = 'Sagittarius';
+    } else {
+        $sign = 'Capricorn';
+    }
+    echo '<div>' . $sign . '</div>';
+}
+
+//This function below calls one of the 12 functions above based on date entered
 function horoscopeCalc($day, $month) {
     switch ($month) {
         case 'jan':
             january($day);
+            break;
+        case 'feb':
+            february($day);
+            break;
+        case 'mar':
+            march($day);
+            break;
+        case 'apr':
+            april($day);
+            break;
+        case 'may':
+            may($day);
+            break;
+        case 'jun':
+            june($day);
+            break;
+        case 'jul':
+            july($day);
+            break;
+        case 'aug':
+            august($day);
+            break;
+        case 'sep':
+            september($day);
+            break;
+        case 'oct':
+            october($day);
+            break;
+        case 'nov':
+            november($day);
             break;
     }
 }
